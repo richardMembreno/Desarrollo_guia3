e4410dfbcf0ef85790dca1ef980a59aecdb0c714
ricardo.membreno <remembreno@gmail.com>
2023-08-23 18:46:05 -0600

    desarrollo guia tres DPS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {Headers} from './components/Header';
+import { ProductList } from './components/ProductList';
 
 function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Headers 
+        allProducts = {allProducts}
+        setAllProducts = {setAllProducts}
+        total = {total}
+        setTotal = {setTotal}
+        countProducts = {countProducts}
+        setCountProducts = {setCountProducts}
+      />
+      <ProductList 
+        allProducts = {allProducts}
+        setAllProducts = {setAllProducts}
+        total = {total}
+        setTotal = {setTotal}
+        countProducts = {countProducts}
+        setCountProducts = {setCountProducts}
+      />
+    </>
   );
 }
 
